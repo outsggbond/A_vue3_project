ec36d1d8b9f6790bc2dcbc4ba4299c6b65bddef1
outsgg <17028712+outsgg@user.noreply.gitee.com>
2026-05-07 21:25:30 +0800

    修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <h1>这是一个完整项目</h1>
+    <el-button type="primary" size="default" :icon="Plus"> 搜索</el-button>
+    <el-button type="success" size="default" :icon="Edit"> 编辑</el-button>
+    <el-button type="danger" size="default" :icon="Delete"> 删除</el-button>
+    <hello-world></hello-world>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+import helloWorld from './components/helloWorld.vue';
 
-<style></style>
+</script>
+
+<style>
+
+</style>
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>这个是一个组件</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style>
+
+</style>
