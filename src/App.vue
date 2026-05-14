9e5a6be2d2100a8795df41541c2420353cf96fb3
outsgg <17028712+outsgg@user.noreply.gitee.com>
2026-05-14 12:22:20 +0800

    api二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-    <h1>测试axios二次封装</h1>
+    <h1>
+      
+    </h1>
   </div>
 </template>
 
-<script setup lang="ts">
-import request from './utils/request'
-import { onMounted } from 'vue'
+<script setup lang="ts"></script>
 
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
-})
-</script>
-<style scoped lang="scss"></style>
+<style></style>
---
+++ src/api/user/index.ts
@@ -0,0 +1,13 @@
+//同意管理项目用户相关的
+import request from "../../utils/request";
+import type { loginForm, loginResponseData,userResponseData } from "./type";
+//统一管理接口
+
+export const API = {
+    LOGIN_URL: "/user/login",
+    USERINFO_URL: "/user/Info"
+} as const;
+
+export const reqLogin = (data: any) => request.post<any, loginResponseData>(API.LOGIN_URL, data)
+
+export const reqUserInfo = () => request.get<any,userResponseData>(API.USERINFO_URL);
